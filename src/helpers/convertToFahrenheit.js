8aae1f8b0f62cfccded85a2662ccd35c8fe495ea
Saikat Das <dassaikat1901@gmail.com>
2024-02-02 10:45:07 +0530

    Implement feature/fix

--- src/helpers/convertToFahrenheit.js
+++ src/helpers/convertToFahrenheit.js
@@ -1,3 +1,9 @@
+/**
+ * Converts a temperature value from Celsius to Fahrenheit.
+ *
+ * @param {number} celsius - The temperature value in Celsius.
+ * @returns {number} The equivalent temperature value in Fahrenheit.
+ */
 export default function convertToFahrenheit(celsius) {
   return (celsius * 9) / 5 + 32;
 }
